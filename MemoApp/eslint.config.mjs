756bf262ad164d44b6d9bb0179530f8c9c55364b
ochasan05 <shuthimu05@gmail.com>
2025-05-16 13:07:49 +0900

    [add] eslint v9

---
+++ MemoApp/eslint.config.mjs
@@ -0,0 +1,28 @@
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+import { defineConfig } from "eslint/config"
+
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"]
+  },
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.node }
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/explicit-function-return-type": ["warn"],
+      "semi": ["error", "never"],
+      "comma-dangle": ["error", "never"]
+    }
+  }
+])
